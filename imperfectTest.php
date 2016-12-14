e61501174e546463c1e1de31b6b890eeace664ff
goju <julienbrandin@sfr.fr>
2016-12-14 12:17:00 +0100

    Deleted comments

--- imperfectTest.php
+++ imperfectTest.php
@@ -1,28 +1,14 @@
 <?php
 //error_reporting(E_ERROR | E_PARSE);
 
-// help
-// generate solution down and left
-// merge with random maze
-/*
-define("wall", "<img src='https://image.freepik.com/icones-gratuites/forme-de-carre-noir_318-35701.jpg' style='width:20px; height: 20px;'>");
-define("corridor", "<img src='http://www.normachats.fr/wp-content/uploads/2015/10/carr%C3%A9-blanc.png' style='width:20px; height: 20px;'>");
-define("solution", "<img src='http://les-nouveaux-voyageurs.com/wp-content/uploads/2015/01/carr%C3%A9-orange.png' style='width:20px; height: 20px;'>");
-*/
 function imperfectMaze($maze_height, $maze_width)
 {
-
-    //$maze_height = 20;
-    //$maze_width = 20;
     $mazeMap = array();
     for ($x = 0; $x < $maze_height; $x++) {
         for ($y = 0; $y < $maze_width; $y++) {
-            //$mazeMap[$x][$y]=1;
             $mazeMap[$x][$y] = rand(0, 1);
         }
     }
-
-//var_dump($mazeMap);
 
     $mazeMap[0][0] = 2;
     $mazeMap[$maze_height - 1][$maze_width - 1] = 0;
@@ -36,52 +22,33 @@ function imperfectMaze($maze_height, $maze_width)
 
     $finish = false;
 
-//while($x <= $maze_height -1 || $y <= $maze_width - 1){
-//while(count($moves)){
     while (!$finish) {
 
         $randomNumber = rand(0, 1);
 
-        //var_dump($directionArray[$randomNumber]);
-
         switch ($directionArray[$randomNumber]) {
             case "S":
-                //if($mazeMap[$x-2][$y]==1 && $x-2!=0 && $x-2!=$maze_height-1) {
-                //if($mazeMap[$x+2][$y]==1 && $x+2!=0 && $x+2!=$maze_height-1) {
-                //$mazeMap[$x + 2][$y] = 2;
                 if ($x > $maze_height)
                     $x = $maze_height - 1;
                 $mazeMap[$x + 1][$y] = 2;
-                //$x += 2;
                 $x += 1;
-                //}
                 break;
             case "E":
-                //if($mazeMap[$x][$y+2]==1 && $y+2!=0 && $y+2!=$maze_width-1) {
-                //$mazeMap[$x][$y + 2] = 2;
                 if ($y > $maze_width)
                     $y = $maze_width - 1;
                 $mazeMap[$x][$y + 1] = 2;
-                //$y += 2;
                 $y += 1;
-                //}
                 break;
         }
 
-        //if($x >= $maze_height && $y >= $maze_width){
         if ($mazeMap[$maze_height - 1][$maze_width - 1] == 2) {
             $finish = true;
         }
 
-
         if ($x > $maze_height + 1 && $y > $maze_width + 1) {
-            //printLaby($mazeMap);
-            echo("exit");
             break;
         }
 
-        //var_dump($x . "   " . $y);
-        //printLaby($mazeMap);
         if ($x >= $maze_height) {
             $x -= 1;
         }
@@ -89,13 +56,7 @@ function imperfectMaze($maze_height, $maze_width)
         if ($y >= $maze_width) {
             $y -= 1;
         }
-
-        //$x++;
-        //$y++;
     }
-
-//var_dump($x);
-//var_dump($y);
 
     printLaby($mazeMap);
 }
--- test.php
+++ test.php
@@ -3,33 +3,29 @@ error_reporting(E_ERROR | E_PARSE);
 
 function perfectMaze($maze_height, $maze_width)
 {
-    //$maze_height = 100;
-    //$maze_width = 100;
     $maze = array();
     $moves = array();
-    $width = $maze_width;
-    $height = $maze_height;
-    for ($x = 0; $x < $height; $x++) {
-        for ($y = 0; $y < $width; $y++) {
+    for ($x = 0; $x < $maze_height; $x++) {
+        for ($y = 0; $y < $maze_width; $y++) {
             $maze[$x][$y] = 1;
         }
     }
     $x_pos = 1;
     $y_pos = 1;
     $maze[$x_pos][$y_pos] = 0;
-    array_push($moves, $y_pos + ($x_pos * $width));
+    array_push($moves, $y_pos + ($x_pos * $maze_width));
     while (count($moves)) {
         $possible_directions = "";
-        if ($maze[$x_pos + 2][$y_pos] == 1 and $x_pos + 2 != 0 and $x_pos + 2 != $height - 1) {
+        if ($maze[$x_pos + 2][$y_pos] == 1 and $x_pos + 2 != 0 and $x_pos + 2 != $maze_height - 1) {
             $possible_directions .= "S";
         }
-        if ($maze[$x_pos - 2][$y_pos] == 1 and $x_pos - 2 != 0 and $x_pos - 2 != $height - 1) {
+        if ($maze[$x_pos - 2][$y_pos] == 1 and $x_pos - 2 != 0 and $x_pos - 2 != $maze_height - 1) {
             $possible_directions .= "N";
         }
-        if ($maze[$x_pos][$y_pos - 2] == 1 and $y_pos - 2 != 0 and $y_pos - 2 != $width - 1) {
+        if ($maze[$x_pos][$y_pos - 2] == 1 and $y_pos - 2 != 0 and $y_pos - 2 != $maze_width - 1) {
             $possible_directions .= "W";
         }
-        if ($maze[$x_pos][$y_pos + 2] == 1 and $y_pos + 2 != 0 and $y_pos + 2 != $width - 1) {
+        if ($maze[$x_pos][$y_pos + 2] == 1 and $y_pos + 2 != 0 and $y_pos + 2 != $maze_width - 1) {
             $possible_directions .= "E";
         }
         if ($possible_directions) {
@@ -56,11 +52,11 @@ function perfectMaze($maze_height, $maze_width)
                     $y_pos += 2;
                     break;
             }
-            array_push($moves, $y_pos + ($x_pos * $width));
+            array_push($moves, $y_pos + ($x_pos * $maze_width));
         } else {
             $back = array_pop($moves);
-            $x_pos = floor($back / $width);
-            $y_pos = $back % $width;
+            $x_pos = floor($back / $maze_width);
+            $y_pos = $back % $maze_width;
         }
     }
 
@@ -77,8 +73,6 @@ function perfectMaze($maze_height, $maze_width)
     $y = 0;
 
     while ($maze[$new_maze_height][$new_maze_width] != 2) {
-
-        //if($maze[$x][$y] == 0){
 
         if ($maze[$x + 1][$y] == 0) {
             $maze[$x + 1][$y] = 2;
@@ -105,39 +99,24 @@ function perfectMaze($maze_height, $maze_width)
             $maze[$x][$y] = 3;
             $x++;
         }
-        //}
-        //printMaze($maze, $height, $width);
     }
 
-    printMaze($maze, $height, $width);
+    printMaze($maze, $maze_height, $maze_width);
 }
 
-function printMaze($maze, $height, $width)
+function printMaze($maze, $maze_height, $maze_width)
 {
-    //define("wall", "<img src='https://image.freepik.com/icones-gratuites/forme-de-carre-noir_318-35701.jpg' style='width:20px; height: 20px;'>");
-    define("wall", "<img src='https://user.oc-static.com/files/347001_348000/347995.png' style='width:20px; height: 20px;'>");
-    //define("corridor", "<img src='http://www.normachats.fr/wp-content/uploads/2015/10/carr%C3%A9-blanc.png' style='width:20px; height: 20px;'>");
-    define("corridor", "<img src='https://s-media-cache-ak0.pinimg.com/originals/d6/c5/ad/d6c5ad469011e4b1ea38e0af687a53f2.jpg' style='width:20px; height: 20px;'>");
-    //define("solution", "<img src='http://les-nouveaux-voyageurs.com/wp-content/uploads/2015/01/carr%C3%A9-orange.png' style='width:20px; height: 20px;'>");
-    define("solution", "<img src='http://www.3d-diablotine.com/tuto/ground017_02.jpg' style='width:20px; height: 20px;'>");
-    define("back", "<img src='http://www.expertmultimedia.ch/ressources/graphisme-symboles-logos/symboles-1/carre-violet/image_preview' style='width:20px; height: 20px;'>");
-
-    for ($x = 0; $x < $height - 1; $x++) {
-        for ($y = 0; $y < $width - 1; $y++) {
+    for ($x = 0; $x < $maze_height - 1; $x++) {
+        for ($y = 0; $y < $maze_width - 1; $y++) {
             if ($maze[$x][$y] == 1) {
-                //echo "#";
                 echo(wall);
             } elseif ($maze[$x][$y] == 2)
                 echo(solution);
-            /*elseif ($maze[$x][$y] == 3)
-                echo(back);
-            */else {
-                //echo "&nbsp;";
+            else {
                 echo(corridor);
             }
         }
         echo "<br>";
     }
-    echo "</code>";
 }
 ?>
